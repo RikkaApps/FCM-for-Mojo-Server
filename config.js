0c3ce3622c617fb3689e8f982c4eeac3f4d3eb8b
Rikka <rikka@xing.moe>
2017-08-25 22:39:12 +0800

    fix: example config.js can really run now / add comment

--- config.js
+++ config.js
@@ -3,20 +3,34 @@ var fs = require('fs');
 var config = {
     "mojo": {
         "webqq": {
+            /* openqq plugin local port */
             "openqq": 5003
         }
     },
+
+    /* local http server port, FFM plugin will send messages to this port */
 	"local_port": 5004,
+
+	/* client config file, must be a valid json, do not need to edit it manually */
 	"client_config": "client.json",
+
+	/* http server port for client */
     "port": 5005,
-    "hostname": "",
-    "basic_auth": {
+
+    // hostname, 0.0.0.0 default (IPv4)
+    /*"hostname": "",*/
+
+    /* basic auth config, see https://github.com/http-auth/http-auth#configurations
+    */
+    /*"basic_auth": {
         "file": ""
-    },
-    "https": {
+    },*/
+
+    // https config, see https://nodejs.org/dist/latest/docs/api/https.html
+    /*"https": {
         "key": fs.readFileSync(""),
         "cert": fs.readFileSync("")
-    }
+    }*/
 }
 
 module.exports = config;
